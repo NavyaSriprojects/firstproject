23c2e3fccd647f98c60e8f3289920ce96acfbde2
NavyaSriprojects <113357760+NavyaSriprojects@users.noreply.github.com>
2022-09-12 18:55:56 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,12 @@
+import py
 p1=input("Enter the name of player1:")
 p2=input("Enter the name of player2:")
-opi=input(p1,"do you want to continue playing?")
+opi=input("p1 do you want to continue playing?")
 if(opi=="yes"):
-   choice=rand.int
+   choice=rand.randint(11)
+   luckydraw=rand.randint(11)
+   if(choice==luckydraw):
+      print("you won")
+   else:
+      print("sorry,you lost!")
+   
