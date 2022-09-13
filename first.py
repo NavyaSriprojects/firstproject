01a1abe7fc6fead9f8c1ad4a5978651c512c3b84
NavyaSriprojects <113357760+NavyaSriprojects@users.noreply.github.com>
2022-09-13 13:10:40 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,12 +1,51 @@
-import random
-p1=input("Enter the name of player1:")
-p2=input("Enter the name of player2:")
-opi=input("p1 do you want to continue playing?")
-if(opi=="yes"):
-   choice=rand.randint(11)
-   luckydraw=rand.randint(11)
-   if(choice==luckydraw):
-      print("you won")
-   else:
-      print("sorry,you lost!")
-   
+
+   from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
